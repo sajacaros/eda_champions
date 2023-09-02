b949eeccc5d27f332ec8231f30b398681c9f68d6
dukim <sajacall82@naver.com>
2023-09-02 19:11:52 +0900

    side profile complete

--- dash/nav.py
+++ dash/nav.py
@@ -2,10 +2,12 @@ import dash_bootstrap_components as dbc
 
 from corr import corr_scatter
 from report import report_player
+from dictionary import explain_terms
 
 
 def tabs(df, app):
     return dbc.Tabs([
         dbc.Tab(report_player(df, app), label='report'),
-        dbc.Tab(corr_scatter(df, app), label='corr_scatter')
+        dbc.Tab(corr_scatter(df, app), label='corr_scatter'),
+        dbc.Tab(explain_terms(), label='dictionary')
     ])
