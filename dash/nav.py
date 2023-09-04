99f071208d9c07d505f16e0a79e54648537778a6
dukim <sajacall82@naver.com>
2023-09-04 10:40:09 +0900

    update

--- dash/nav.py
+++ dash/nav.py
@@ -7,7 +7,7 @@ from dictionary import explain_terms
 
 def tabs(df, app):
     return dbc.Tabs([
-        dbc.Tab(corr_scatter(df, app), label='corr_scatter'),
+        dbc.Tab(corr_scatter(df, app), label='correlation'),
         dbc.Tab(report_player(df, app), label='report'),
         dbc.Tab(explain_terms(), label='dictionary')
     ])
