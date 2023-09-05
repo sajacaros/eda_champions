a68415eda018c105e2c2042094c3d3afe4d8959a
dukim <sajacall82@naver.com>
2023-09-05 13:26:10 +0900

    update density curve

--- report/nav.py
+++ report/nav.py
@@ -9,8 +9,8 @@ from numeric_analysis import numeric_analysis
 
 def tabs(df, app):
     return dbc.Tabs([
-        dbc.Tab(corr_scatter(df, app), label='Correlation'),
         dbc.Tab(numeric_analysis(df, app), label='Numeric'),
+        dbc.Tab(corr_scatter(df, app), label='Correlation'),
         dbc.Tab(report_player(df, app), label='Report'),
         dbc.Tab(report_team(df, app), label='Team'),
         dbc.Tab(explain_terms(), label='Dictionary')
