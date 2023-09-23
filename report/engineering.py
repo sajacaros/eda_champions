761bd5df271c63c4249aa790724e588a153fbd35
dukim <sajacall82@naver.com>
2023-09-24 04:40:58 +0900

    feat: load final csv
    최종 csv 파일이 존재하면 csv file에서 로드
    없으면 csv 파일들로부터 머지하고 최종 csv 파일을 만듬

--- report/engineering.py
+++ report/engineering.py
@@ -1,8 +1,23 @@
+import os.path
+
 import pandas as pd
 
 
 def get_player(parent_path):
     return pd.read_csv(f'{parent_path}/new/players_all.csv').drop('Unnamed: 0', axis=1)
+
+
+def generate_eda_path(parent_path):
+    return f'{parent_path}/new/eda_all.csv'
+
+
+def save_df(parent_path, df):
+    df.to_csv(generate_eda_path(parent_path), index=False)
+    return df
+
+
+def exists_final_csv(parent_path):
+    return os.path.exists(generate_eda_path(parent_path))
 
 
 def get_xbet(parent_path):
@@ -54,13 +69,19 @@ def feature_check(eda_df):
 
 
 def get_data(parent_path='../data'):
-    # plyaer data load 및 정리(프로필)
-    player_df = get_player(parent_path)
-    # 1xbet data load 및 정리(스텟)
-    xbet_df = get_xbet(parent_path)
-    # understat data load 및 정리(스텟)
-    understat_df = get_understat(parent_path)
-    # capology load(연봉)
-    capology_df = get_capology(parent_path)
-    eda_df = merge(player_df, xbet_df, understat_df, capology_df)
-    return feature_check(eda_df)
+    if exists_final_csv(parent_path):
+        print(f'load final csv file, file : {generate_eda_path(parent_path)}')
+        return pd.read_csv(generate_eda_path(parent_path))
+    else:
+        print(f'start to merge csv files')
+        # plyaer data load 및 정리(프로필)
+        player_df = get_player(parent_path)
+        # 1xbet data load 및 정리(스텟)
+        xbet_df = get_xbet(parent_path)
+        # understat data load 및 정리(스텟)
+        understat_df = get_understat(parent_path)
+        # capology load(연봉)
+        capology_df = get_capology(parent_path)
+        eda_df = merge(player_df, xbet_df, understat_df, capology_df)
+        print(f'complete to merge csv files, save to csv({generate_eda_path(parent_path)})')
+        return save_df(parent_path, feature_check(eda_df))
